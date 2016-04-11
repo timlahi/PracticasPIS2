13ab234259850f17083f0d2d77ff1b3dce8423b9
tarikk <tarikimlahi@outlook.es>
2016-04-11 18:51:32 +0200

    Buscando Detalles

--- EjemploCapitulo1/src/capitulo1/Rental.java
+++ EjemploCapitulo1/src/capitulo1/Rental.java
@@ -1,43 +1,47 @@
 package capitulo1;
 
 class Rental {
-private Movie _movie;
-private int _daysRented;
-public Rental(Movie movie, int daysRented) {
-_movie = movie;
-_daysRented = daysRented;
-}
-public int getDaysRented() {
-return _daysRented;
-}
-public Movie getMovie() {
-return _movie;
-}
+	private Movie _movie;
+	private int _daysRented;
 
-int getFrequentRenterPoints() {
-if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-getDaysRented() > 1)
-return 2;
-else
-return 1;
-}
-int getCharge() { //Hemos movido este método desde la clase Customer
-	int result = 0;
-	switch (getMovie().getPriceCode()) {
-	case Movie.REGULAR:
-		result += 2;
-		if (getDaysRented() > 2)
-			result += (getDaysRented() - 2) * 1.5;
-		break;
-	case Movie.NEW_RELEASE:
-		result += getDaysRented() * 3;
-		break;
-	case Movie.CHILDRENS:
-		result += 1.5;
-		if (getDaysRented() > 3)
-			result += (getDaysRented() - 3) * 1.5;
-		break;
+	public Rental(Movie movie, int daysRented) {
+		_movie = movie;
+		_daysRented = daysRented;
 	}
-	return result;
-}
+
+	public int getDaysRented() {
+		return _daysRented;
+	}
+
+	public Movie getMovie() {
+		return _movie;
+	}
+
+	int getFrequentRenterPoints() {
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
+				&& getDaysRented() > 1)
+			return 2;
+		else
+			return 1;
+	}
+
+	double getCharge() { // Hemos movido este método desde la clase Customer
+		double result = 0;
+		switch (getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			result += 2;
+			if (getDaysRented() > 2)
+				result += (getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			result += getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			result += 1.5;
+			if (getDaysRented() > 3)
+				result += (getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return result;
+	}
 }
--- EjemploCapitulo1/test/capitulo1/TestR.java
+++ EjemploCapitulo1/test/capitulo1/TestR.java
@@ -10,7 +10,7 @@ import junit.framework.TestCase;
 
 public class TestR extends TestCase {
 
-	   @Test
+	   /*@Test
 	   public void test() {
 	       Movie movie1 = new Movie("Monty Python and the Holy Grail", 0);
 	       Movie movie2 = new Movie("Ice Age", 2);
@@ -25,6 +25,41 @@ public class TestR extends TestCase {
 	       String res = "Rental Record for "+"Isa"+"\n"+"   Monty Python and the Holy Grail 14.0"+"\n"+"   Ice Age 1.5"+"\n"+
 	       "Amount owed is 15.5"+"\n"+"You earned 2 frequent renter points";
 	       String mio = yo.statement();
-	       assertEquals(res,mio);
+	       assertEquals(res,mio);*/
+	
+	@Test
+	public void test() {
+		Movie movie1 = new Movie("Monty Python and the Holy Grail", 0);
+		Movie movie2 = new Movie("Ice Age", 2);
+		Movie movie3 = new Movie("Batman vs Superman", 1);
+		Movie movie4 = new Movie("Rey Leon", 2);
+		Movie movie5 = new Movie("Resucitado", 0);
+		
+
+		Customer yo = new Customer("Miguel");
+		Rental r1 = new Rental(movie1, 10);
+		Rental r2 = new Rental(movie2, 2);
+		Rental r3 = new Rental(movie3, 1);
+		Rental r4 = new Rental(movie4, 10);
+		Rental r5 = new Rental(movie5, 10);
+		
+		movie5.setPriceCode(1);
+		yo.addRental(r1);
+		yo.addRental(r2);
+		yo.addRental(r3);
+		yo.addRental(r4);
+		yo.addRental(r5);
+
+		String res = "Rental Record for " + "Miguel" + 
+					"\n" + "\t" + "Monty Python and the Holy Grail 14.0" +
+					"\n" + "\t" + "Ice Age 1.5" + 
+					"\n" + "\t" + "Batman vs Superman 3.0" +
+					"\n" + "\t" + "Rey Leon 12.0" +
+					"\n" + "\t" + "Resucitado 30.0" +
+					"\n" + "Amount owed is 60.5" +
+					"\n" + "You earned 6.0 frequent renter points";
+
+		assertEquals(res, yo.statement());
+	
 }
 }
